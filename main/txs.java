18939f45122eba45273faf258123861801de0aaf
Zux2811 <125178485+Zux2811@users.noreply.github.com>
2023-12-22 17:02:21 +0700

    Update txs.java

--- main/txs.java
+++ main/txs.java
@@ -3,107 +3,102 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class LibraryManager {
-
-    private static Connection getConnection() throws SQLException {
-        // Kết nối đến cơ sở dữ liệu
-
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/Quanlydean", "root", "123456789");
+public class QuanLyThuVienDAO {
+    private Connection getConnection() throws SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/Quanlythuvien";
+        String username = "root";
+        String password = "123456789";
+        return DriverManager.getConnection(jdbcURL, username, password);
     }
 
-    // Hàm thêm sách vào cơ sở dữ liệu
-    public static void addBook(String maSach, String tenSach, String tenTacGia, String maNhaXuatBan, double giaVon) {
+    // Thêm sách
+    public void themSach(String maSach, String tenSach, String tenTacGia, String maNXB, double giaVon) {
         try (Connection connection = getConnection()) {
-            String sql = "INSERT INTO sach (ma_sach, ten_sach, ten_tac_gia, ma_nha_xuat_ban, gia_von) VALUES (?, ?, ?, ?, ?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, maSach);
-                preparedStatement.setString(2, tenSach);
-                preparedStatement.setString(3, tenTacGia);
-                preparedStatement.setString(4, maNhaXuatBan);
-                preparedStatement.setDouble(5, giaVon);
-
-                preparedStatement.executeUpdate();
+            String sql = "INSERT INTO sach (ma_sach, ten_sach, ten_tac_gia, ma_nxb, gia_von) VALUES (?, ?, ?, ?, ?)";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, maSach);
+                statement.setString(2, tenSach);
+                statement.setString(3, tenTacGia);
+                statement.setString(4, maNXB);
+                statement.setDouble(5, giaVon);
+                statement.executeUpdate();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    // Hàm đọc thông tin tất cả sách từ cơ sở dữ liệu
-    public static List<Book> getAllBooks() {
-        List<Book> books = new ArrayList<>();
+    // Xem danh sách sách
+    public void xemDanhSachSach() {
         try (Connection connection = getConnection()) {
             String sql = "SELECT * FROM sach";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
                     while (resultSet.next()) {
-                        Book book = new Book(
-                                resultSet.getString("ma_sach"),
-                                resultSet.getString("ten_sach"),
-                                resultSet.getString("ten_tac_gia"),
-                                resultSet.getString("ma_nha_xuat_ban"),
-                                resultSet.getDouble("gia_von")
-                        );
-                        books.add(book);
+                        System.out.println("Mã sách: " + resultSet.getString("ma_sach"));
+                        System.out.println("Tên sách: " + resultSet.getString("ten_sach"));
+                        System.out.println("Tên tác giả: " + resultSet.getString("ten_tac_gia"));
+                        System.out.println("Mã NXB: " + resultSet.getString("ma_nxb"));
+                        System.out.println("Giá vốn: " + resultSet.getDouble("gia_von"));
+                        System.out.println("------------------------");
                     }
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return books;
     }
 
-    // Hàm cập nhật thông tin sách trong cơ sở dữ liệu
-    public static void updateBook(String maSach, String tenSach, String tenTacGia, String maNhaXuatBan, double giaVon) {
+    // Sửa thông tin sách
+    public void suaThongTinSach(String maSach, String tenSach, String tenTacGia, String maNXB, double giaVon) {
         try (Connection connection = getConnection()) {
-            String sql = "UPDATE sach SET ten_sach = ?, ten_tac_gia = ?, ma_nha_xuat_ban = ?, gia_von = ? WHERE ma_sach = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, tenSach);
-                preparedStatement.setString(2, tenTacGia);
-                preparedStatement.setString(3, maNhaXuatBan);
-                preparedStatement.setDouble(4, giaVon);
-                preparedStatement.setString(5, maSach);
-
-                preparedStatement.executeUpdate();
+            String sql = "UPDATE sach SET ten_sach = ?, ten_tac_gia = ?, ma_nxb = ?, gia_von = ? WHERE ma_sach = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, tenSach);
+                statement.setString(2, tenTacGia);
+                statement.setString(3, maNXB);
+                statement.setDouble(4, giaVon);
+                statement.setString(5, maSach);
+                statement.executeUpdate();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    // Hàm xóa sách khỏi cơ sở dữ liệu
-    public static void deleteBook(String maSach) {
+    // Xóa sách
+    public void xoaSach(String maSach) {
         try (Connection connection = getConnection()) {
             String sql = "DELETE FROM sach WHERE ma_sach = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, maSach);
-
-                preparedStatement.executeUpdate();
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, maSach);
+                statement.executeUpdate();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static class Book {
-        private String maSach;
-        private String tenSach;
-        private String tenTacGia;
-        private String maNhaXuatBan;
-        private double giaVon;
+    public static void main(String[] args) {
+        QuanLyThuVienDAO dao = new QuanLyThuVienDAO();
 
-        public Book(String maSach, String tenSach, String tenTacGia, String maNhaXuatBan, double giaVon) {
-            this.maSach = maSach;
-            this.tenSach = tenSach;
-            this.tenTacGia = tenTacGia;
-            this.maNhaXuatBan = maNhaXuatBan;
-            this.giaVon = giaVon;
-        }
+        // Thêm sách
+        dao.themSach("S001", "Java Programming", "John Doe", "NXB001", 150000);
 
+        // Xem danh sách sách
+        dao.xemDanhSachSach();
 
+        // Sửa thông tin sách
+        dao.suaThongTinSach("S001", "Java Programming (Updated)", "John Doe", "NXB001", 180000);
+
+        // Xem danh sách sách sau khi sửa
+        dao.xemDanhSachSach();
+
+        // Xóa sách
+        dao.xoaSach("S001");
+
+        // Xem danh sách sách sau khi xóa
+        dao.xemDanhSachSach();
     }
 }
